f1ea83b4338f1d002a9b2cb94613025698c76280
Ed Presutti <epresutti@gmail.com>
2018-08-15 14:25:45 -0500

    - Modified script to create parameter file

--- scripts/pam7/genpam7keys.sh
+++ scripts/pam7/genpam7keys.sh
@@ -12,10 +12,14 @@ error_exit() {
 	exit "${code}"
 }
 
-if [ $# -ne 2 ]; then
-        echo "Usage : $0 <namespace name> <keystore password>"
+if [ $# -lt 2 ]; then
+        echo "Usage : $0 <namespace name> <keystore password> <generate env file for oc new-app>"
         echo ""
-        echo "Example : \"$0 rhpam7demo redhat123\" will create the keystore files and generate secrets in the rhpam7demo namespace, with a password of redhat123."
+	echo "Namespace : The Openshift project to use for secret generation"
+	echo "Keystore Password : The password to embed into the keystore"
+	echo "Generate ENV : If this parameter is set to \"yes\" then the script will generate an ENV file that can be passed to \"oc new-app\""
+	echo ""
+        echo "Example : \"$0 rhpam7demo redhat123 yes\" will create the keystore files and generate secrets in the rhpam7demo namespace, with a password of redhat123."
 	echo ""
 	echo "Note : keytool and oc *must* be in the path for this script to execute properly. Please log in to oc before running this script"
 	echo ""
@@ -30,6 +34,8 @@ else
 	BCSECRETNAME="businesscentral-app-secret"
 	BCSTOREFN="businesscentral.keystore"
 	DNAME="CN=demo.redhat.com, OU=Lab, O=RedHat, L=Raleigh, S=NC, C=US"
+
+	GENENVFILE=`echo $3 | tr '[:upper:]' '[:lower:]'`
 
 	trap 'error_exit ${LINENO}' ERR
 
@@ -60,4 +66,25 @@ else
 	echo "KIE Server Certificate Name : ${KIESECRETNAME}"
 	echo "KIE Server Keystore Password : ${KPASS}"
 
+	if [ "${GENENVFILE}" = "yes" ]; then
+		ENVFILE="rhpamauth.env"
+		echo "Generating environment file : ${ENVFILE}"
+		echo "APPLICATION_NAME=\"rhpam7authdemo\"" > ${ENVFILE}
+		echo "KIE_ADMIN_USER=\"bpmsadmin\"" >> ${ENVFILE}
+		echo "KIE_ADMIN_PWD=\"${KPASS}\"" >> ${ENVFILE}
+		echo "KIE_SERVER_CONTROLLER_USER=\"controllerUser\"" >> ${ENVFILE}
+		echo "KIE_SERVER_CONTROLLER_PWD=\"${KPASS}\"" >> ${ENVFILE}
+		echo "KIE_SERVER_USER=\"executionUser\"" >> ${ENVFILE}
+		echo "KIE_SERVER_PWD=\"${KPASS}\"" >> ${ENVFILE}
+		echo "ADMIN_USERNAME=\"eapadmin\"" >> ${ENVFILE}
+		echo "ADMIN_PASSWORD=\"${KPASS}\"" >> ${ENVFILE}
+		echo "BUSINESS_CENTRAL_HTTPS_SECRET=\"businesscentral-app-secret\"" >> ${ENVFILE}
+		echo "BUSINESS_CENTRAL_HTTPS_KEYSTORE=\"businesscentral.keystore\"" >> ${ENVFILE}
+		echo "BUSINESS_CENTRAL_HTTPS_NAME=\"businesscentral-app-secret\"" >> ${ENVFILE}
+		echo "BUSINESS_CENTRAL_HTTPS_PASSWORD=\"${KPASS}\"" >> ${ENVFILE}
+		echo "KIE_SERVER_HTTPS_SECRET=\"kieserver-app-secret\"" >> ${ENVFILE}
+		echo "KIE_SERVER_HTTPS_KEYSTORE=\"kieserver.keystore\"" >> ${ENVFILE}
+		echo "KIE_SERVER_HTTPS_NAME=\"kieserver-app-secret\"" >> ${ENVFILE}
+		echo "KIE_SERVER_HTTPS_PASSWORD=\"${KPASS}\"" >> ${ENVFILE}
+	fi
 fi
