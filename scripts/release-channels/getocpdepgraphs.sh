a7e8b71c95f1aa0aab29b8c443c80edc5c568f27
Ed Presutti <epresutt@eds-macbook-pro.internal.unixwarhammer.com>
2020-08-10 11:57:37 -0500

    Updating for new channels

--- scripts/release-channels/getocpdepgraphs.sh
+++ scripts/release-channels/getocpdepgraphs.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-RELEASECHANNELS=('stable-4.1' 'stable-4.2' 'stable-4.3' 'stable-4.4' 'fast-4.2' 'fast-4.3' 'fast-4.4' 'candidate-4.3' 'candidate-4.4')
+RELEASECHANNELS=(
+	'stable-4.1' 
+	'stable-4.2' 
+	'stable-4.3' 
+	'stable-4.4' 
+	'stable-4.5' 
+	'fast-4.2' 
+	'fast-4.3' 
+	'fast-4.4' 
+	'fast-4.5' 
+	'candidate-4.3' 
+	'candidate-4.4'
+	'candidate-4.5')
+
 OUTPUTDIR=./graphs
 
 mkdir -p ${OUTPUTDIR}
